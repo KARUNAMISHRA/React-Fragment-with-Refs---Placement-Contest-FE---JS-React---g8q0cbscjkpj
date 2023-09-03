ba83ff811646934bf7761a6ddc1a432529125f12
karuna sagar mishra <112199069+KARUNAMISHRA@users.noreply.github.com>
2023-09-03 19:24:03 +0530

    Update ListItems.js

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,6 +1,6 @@
-import React from "react";
+ import React from "react";
 
 const ListItems = (props) => {
-  return <li className="items">List element number {props.valuex}</li>;
+  return <li className={props.valuex===1?"items":""}>List element number {props.valuex}</li>;
 };
 export default ListItems;
